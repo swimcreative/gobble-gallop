e9b2a2cc8db33792dc01fd737ef5540b8aaa2f2d
Peter Sahlberg <pete@swimcreative.com>
2017-11-01 16:36:05 -0500

    post launch tweaks

--- inc/acf/presented-by.php
+++ inc/acf/presented-by.php
@@ -1,5 +1,24 @@
-<?php $sponsors = get_field('presenting_sponsors');
+<!--<?php $sponsors = get_field('presenting_sponsors');
 if(have_rows('presenting_sponsors')) : ?>
+<section id="presented-by">
+  <div class="inner-wrap">
+    <div class="header-container">
+      <h6>Presented By</h6>
+    </div>
+      <div class="sponsor_logo-container">
+
+        <?php foreach($sponsors as $sponsor) : ?>
+
+        <a target="_blank" href="<?php echo $sponsor['sponsor_link']; ?>" class="sponsor_logo"><img src="<?php echo $sponsor['sponsor_logo']; ?>"/></a>
+
+      <?php endforeach; ?>
+     </div>
+  </div>
+</section>
+
+<?php endif; ?> -->
+
+
 <section id="presented-by">
   <div class="inner-wrap">
     <div class="header-container">
@@ -7,13 +26,42 @@ if(have_rows('presenting_sponsors')) : ?>
     </div>
       <div class="logo-container">
 
-        <?php foreach($sponsors as $sponsor) : ?>
+        <?php
 
-        <a target="_blank" href="<?php echo $sponsor['link']; ?>" class="logo"><img src="<?php echo $sponsor['logo']; ?>"/></a>
+        $gobble_page = 5;
+      	$tough_page = 45;
+      	$giddy_page = 47;
 
-      <?php endforeach; ?>
+
+        $sponsors = get_field('sponsors', 40);
+        if(have_rows('sponsors', 40)) : ?>
+          <?php
+
+          foreach ($sponsors as $s) :
+          $gobble = $s['presenting_sponsor'][0];
+          if( $gobble == 'gobble') : ?>
+          <?php if(is_front_page() || is_page($gobble_page)) : ?>
+            <a target="_blank" href="<?php echo $s['sponsor_link']; ?>" class="logo"><img src="<?php echo $s['sponsor_logo']; ?>"/></a>
+          <?php endif; endif;  endforeach; ?>
+
+          <?php
+          foreach ($sponsors as $s) :
+          $gobble = $s['presenting_sponsor'][0];
+          if($gobble == 'tough') : ?>
+            <?php if(is_page($tough_page)) : ?>
+              <a target="_blank" href="<?php echo $s['sponsor_link']; ?>" class="logo"><img src="<?php echo $s['sponsor_logo']; ?>"/></a>
+          <?php  endif; endif;  endforeach; ?>
+
+          <?php
+          foreach ($sponsors as $s) :
+          $gobble = $s['presenting_sponsor'][0];
+          if($gobble == 'giddy') : ?>
+            <?php if(is_page($giddy_page)) : ?>
+              <a target="_blank" href="<?php echo $s['sponsor_link']; ?>" class="logo"><img src="<?php echo $s['sponsor_logo']; ?>"/></a>
+          <?php endif ;endif;  endforeach; ?>
+
+        <?php endif; ?>
+
      </div>
   </div>
 </section>
-
-<?php endif; ?>
