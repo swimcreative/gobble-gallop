d7e68b214ee4ae7b21ab78d3b111c687f4b3d210
Peter Sahlberg <pete@swimcreative.com>
2017-11-22 14:50:04 -0600

    Updated date in header to update to 2018 and so on...

--- components/header/site-branding.php
+++ components/header/site-branding.php
@@ -11,7 +11,7 @@
 
 	$description = get_bloginfo( 'description', 'display' );
 	if ( $description || is_customize_preview() ) : ?>
-		<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+		<p class="site-description"><?php echo 'Thanksgiving '.date('Y') /* WPCS: xss ok. */ ?></p>
 	<?php
 endif; ?>
 </div><!-- .site-branding -->
